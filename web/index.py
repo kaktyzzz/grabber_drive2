98f58f5bda52a0f370fde1f4e1a0aa7c94a01df3
ktz <kaktyzzz89@gmail.com>
2017-02-02 17:57:07 +0300

    [add] web

---
+++ web/index.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from bottle import route, run, template, static_file
+
+
+
+@route('/hello/<name>')
+def test(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+@route('/')
+def index():
+    return template('index', name='Формула драйва')
+
+@route('/<filename:path>')
+def bootstrap(filename):
+    return static_file(filename, root='bootstrap')
+
+run(host='localhost', port=8080)
